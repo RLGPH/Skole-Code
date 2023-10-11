8b752d3804ff864ceae7367fd0c7653278ef382f
Casper Simon Jensen <casp142b@elev.techcollege.dk>
2023-10-11 08:58:48 +0200

    idk what changed

--- Pizza menu/Pizza menu/obj/Debug/net7.0-windows/checkout.g.i.cs
+++ Pizza menu/Pizza menu/obj/Debug/net7.0-windows/checkout.g.i.cs
@@ -79,13 +79,13 @@ namespace Pizza_menu {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.12.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Pizza menu;component/checkout.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Pizza menu;V1.0.0.0;component/checkout.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\checkout.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -95,7 +95,7 @@ namespace Pizza_menu {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.12.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
