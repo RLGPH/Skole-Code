783e0de85105032bb7b83ffb21de21fbc8047f01
Casper Simon Jensen <casp142b@elev.techcollege.dk>
2024-01-17 12:03:06 +0100

    Added some classes and some UI

---
+++ Library For Games/Library For Games/Games.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_For_Games
+{
+    public class Games
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Hours {  get; set; }
+    }
+}
