c8c27be0dd0a1ca6a61c44f6e6dbeb57ebd71d45
Casper Simon Jensen <casp142b@elev.techcollege.dk>
2024-01-18 12:26:23 +0100

    Update to classes to "finish" them

--- Library For Games/Library For Games/Games.cs
+++ Library For Games/Library For Games/Games.cs
@@ -8,8 +8,16 @@ namespace Library_For_Games
 {
     public class Games
     {
+        public int ID {  get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public int Hours {  get; set; }
+        
+        public Games(string gamename, string description,int hours)
+        {
+            Name = gamename;
+            Description = description;
+            Hours = hours;
+        }
     }
 }
--- Library For Games/Library For Games/Library.cs
+++ Library For Games/Library For Games/Library.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Library_For_Games
 {
-    class Library
+    public class Library
     {
+        public int ID { get; set; }
+        public int Epic {  get; set; }
+        public int Steam { get; set; }
+        public int Other {  get; set; }
+        public Library(int id,int epic, int steam,int other) 
+        { 
+            ID = id;
+            Epic = epic;
+            Steam = steam;
+            Other = other;
+        }
     }
 }
