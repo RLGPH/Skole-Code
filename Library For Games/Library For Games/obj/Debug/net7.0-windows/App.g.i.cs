abbae6773e536edbc537fd52516b1fb03c812eba
Casper Simon Jensen <casp142b@elev.techcollege.dk>
2024-01-19 10:19:19 +0100

    it didnt seem to commit right

--- Library For Games/Library For Games/obj/Debug/net7.0-windows/App.g.i.cs
+++ Library For Games/Library For Games/obj/Debug/net7.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Library_For_Games {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.13.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Library_For_Games {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.13.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
         public static void Main() {
             Library_For_Games.App app = new Library_For_Games.App();
             app.InitializeComponent();
--- Library For Games/Library For Games/obj/Debug/net7.0-windows/Library For Games.AssemblyInfo.cs
+++ Library For Games/Library For Games/obj/Debug/net7.0-windows/Library For Games.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library For Games")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bae467f594c32e366188e4812580b6d0d51fd0c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library For Games")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library For Games")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
