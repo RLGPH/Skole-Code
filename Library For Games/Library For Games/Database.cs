260e555a0378f5223a279d9770dea6cf40741b7f
Casper Simon Jensen <casp142b@elev.techcollege.dk>
2024-01-22 08:00:27 +0100

    updates for the temp version of database

--- Library For Games/Library For Games/Database.cs
+++ Library For Games/Library For Games/Database.cs
@@ -7,17 +7,21 @@ using System.Threading.Tasks;
 
 namespace Library_For_Games
 {
-    public List<Game_S> videogames = new();
+    
     public class Database
     {
-        public void GameUpdateList(Game_S game) 
+        public List<Game_S> game_s = new();
+        public List<Game_S> GameAddList(Game_S game) 
         {
+            game_s = new List<Game_S>();
             int GameID = game.ID;
             string GName = game.Name;
             string GDescrip = game.Description;
             int GHours = game.Hours;
             int FkeyID = game.ForignkeyID;
-
+            Game_S game_S = new(GameID, GName, GDescrip, GHours, FkeyID);
+            game_s.Add(game_S);
+            return game_s;
         }
     }
 }
