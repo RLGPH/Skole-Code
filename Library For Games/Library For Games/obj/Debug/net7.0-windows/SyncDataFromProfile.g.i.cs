c4d5a7938ec40b75ff960a9b3a115574fbe4b227
Casper Simon Jensen <casp142b@elev.techcollege.dk>
2024-02-15 07:15:01 +0100

    update to sync data and database

--- Library For Games/Library For Games/obj/Debug/net7.0-windows/SyncDataFromProfile.g.i.cs
+++ Library For Games/Library For Games/obj/Debug/net7.0-windows/SyncDataFromProfile.g.i.cs
@@ -77,7 +77,7 @@ namespace Library_For_Games {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Library For Games;component/syncdatafromprofile.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Library For Games;V1.0.0.0;component/syncdatafromprofile.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\SyncDataFromProfile.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
